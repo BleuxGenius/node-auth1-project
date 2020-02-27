c9fff1d40dbfd98a0b5f272c697da8489e5b025f
Dani Blackwell <bleuxgenius@gmail.com>
2020-02-26 21:59:03 -0600

    new updates

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,7 @@
-const bcrypt = require('bcryptjs');
-
-const UserData = require('../Users/users-model.js');
-
-module.exports = function restricted(req, res, next) {
-    const { username, password} = req.headers;
-
-    if (username && password) {
-        UserData.findBy({ username})
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'you shall not pass'})
-            }
-        })
-        .catch(error => {
-            res.status(500).json(error)
-        })
-    } else {
-        res.status(400).json({ message: 'need credentials'})
-    }
-}
+module.exports = (req, res, next) => {
+	if (req.session && req.session.loggedIn) {
+		next();
+	} else {
+		res.status(401).json({ you: 'not logged in!' });
+	}
+};
--- server.js
+++ server.js
@@ -1,36 +1,46 @@
-// second file to set up 
-
 const express = require('express');
-const session = ('express-session');
-
-const config = require('./db-config.js')
-
-const userRouter = require('./Users/users-router.js');
-const authrouter = require('./auth/auth-router.js');
-
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexStore = require('connect-session-knex')(session); // remember to curry & pass session
+const authRouter = require('./auth/auth-router.js');
+const apiRouter = require('./api/api-router.js');
+const usersRouter = require('./Users/users-router.js');
+const configureMiddleware = require('./api/configure-middleware.js');
+const restricted = require('./auth/restricted-middleware.js');
+const knex = require('./db-config.js'); // stores sessions in db
 const server = express();
 
-// const sessionConfig = {
-//     name: 'monkey', // sid
-//     secret: 'keep it a secret, keep it safe!',
-//     cookie: {
-//         maxAge: 1000 * 30, // in miliseconds 
-//         secure: false, // can i send this over a secure connection , can be false . true in production
-//         httpOnly: true // can not be accesed from javscript. set to true will not be availabale for the client 
-//     },
-//     resave: false,
-//     saveUninititalized: false, // recreate a session if it has not changed , 
-//     // for GDPR laws against setting cookies automatically 
-// };
 
-server.use(express.json()); // parse the JSON
-// server.use(session(sessionConfig))
+const sessionConfig = {
+	name: 'monster',
+	secret: 'keep it secret, keep it safe!',
+	resave: false,
+	saveUninitialized: true, // GDPR compliance
+	cookie: {
+		maxAge: 1000 * 60 * 10,
+		secure: false, // should be true in production
+		httpOnly: true // true = can't touch cookie
+	},
+	store: new KnexStore({
+		knex,
+		tablename: 'sessions',
+		createtable: true,
+		sidfieldname: 'sid',
+		clearInterval: 1000 * 60 * 15
+	})
+};
 
-server.use('/api/users', userRouter);
-server.use('/api/auth', authrouter);
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig)); // turn on session MW
+server.use('/api/', authRouter);
+server.use('/api/users', restricted, usersRouter);
 
-server.get('/', (req, res)=> {
-    res.json({ api: 'up' });
+
+server.get('/', (req, res) => {
+	// console.log(req.session);
+	res.json({ api: 'up' });
 });
-
 module.exports = server;
